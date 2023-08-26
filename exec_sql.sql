f53028ff35ec62757bae67acc9282166a7de2ca5
Pierre Forstmann <pierre.forstmann@free.fr>
2023-08-26 16:59:54 +0200

    fixed exec_sql.sql and added test_exec_sql.sql

--- exec_sql.sql
+++ exec_sql.sql
@@ -18,12 +18,21 @@ create or replace procedure exec_sql(p_ownname varchar2, p_sql_id varchar2)
 -- needs statistics_level=all
 --
 is
+ORA1008_detected EXCEPTION;
+PRAGMA EXCEPTION_INIT(ORA1008_detected, -1008);
 v_cn integer;
 v_rp integer;
 v_st clob;
 v_nb integer;
+v_pos integer;
+v_name varchar2(30);
+v_type integer;
+v_number number;
+v_date date;
+v_varchar varchar2(128);
 begin
  execute immediate('alter session set current_schema=' || p_ownname);
+ dbms_output.put_line('INFO: alter session set current_schema=' || p_ownname || ' OK.');
 --
  v_cn := dbms_sql.open_cursor;
 --
@@ -35,22 +44,52 @@ begin
 --
  select count(*) into v_nb 
  from sys.v_$sql_bind_capture sbc
- where sbc.sql_id = p_sql_id; 
- dbms_output.put_line('found : ' || v_nb || ' bind variables');
+ where sbc.sql_id = p_sql_id 
+ and child_number = 0;
+ dbms_output.put_line('INFO: found ' || v_nb || ' bind variables for SQL_ID: ' || p_sql_id);
+ for idx in 1 .. v_nb
+ loop
+  select position, name, datatype
+  into v_pos, v_name, v_type
+  from  sys.v_$sql_bind_capture sbc
+  where sbc.sql_id = p_sql_id
+  and position = idx
+  and child_number = 0;
+  dbms_output.put_line('INFO: variable at position: ' || v_pos || ' is named: ' || v_name || ' and has datatype: ' || v_type);
+  if (v_type = 1)
+  then
+   dbms_output.put_line('INFO: binding ' || v_name || ' ...');
+   dbms_sql.bind_variable(v_cn, v_name, 'OK');
+   dbms_output.put_line('INFO:  ... done.' );
+  end if;
+  if (v_type = 2)
+  then
+   dbms_output.put_line('INFO: binding ' || v_name || ' ...');
+   dbms_sql.bind_variable(v_cn, v_name, 0);
+   dbms_output.put_line('INFO:  ... done.' );
+  end if;
+  if (v_type = 12)
+  then
+   dbms_output.put_line('INFO: binding ' || v_name || ' ...');
+   dbms_sql.bind_variable(v_cn, v_name, sysdate);
+   dbms_output.put_line('INFO:  ... done.' );
+  end if;
+ end loop;
 --
--- hard code bind variables for SQL statements in PL/SQL
---
- if (v_nb = 0)
- then
-  null; 
- end if;
---
+ dbms_output.put_line('INFO: executing: ' || v_st || ' ...');
  v_rp := dbms_sql.execute(v_cn);
+ dbms_output.put_line('INFO: ... done.');
 --
  dbms_sql.close_cursor(v_cn);
 --
 exception
+     --
+ when ORA1008_detected then
+     begin
+      dbms_output.put_line('WARNING: ORA-1008 detected.');
+     end;
  when others then
+     dbms_output.put_line('ERROR: unexpected exception ' || SQLCODE || ':' || SQLERRM);
      dbms_sql.close_cursor(v_cn);
      raise;
 --
---
+++ test_exec_sql.sql
@@ -0,0 +1,45 @@
+--
+-- test_exec_sql.sql
+--
+set echo on
+show con_name
+show user
+-- 
+drop table t1 purge;
+drop table t2 purge;
+drop table t3 purge;
+--
+whenever sqlerror exit failure
+--
+create table t1(x1 number);
+create table t2(x2 varchar2(10));
+create table t3(x3 date);
+--
+create or replace procedure pl
+as
+ c int;
+begin
+ select count(*) into c from t1, t2, t3
+ where x1 = 1 or x2 = '2' or x3 = sysdate;
+end;
+/
+show errors
+--
+create or replace procedure pb
+as
+ c int;
+ v1 int := 1;
+ v2 varchar2(10) := '2';
+ v3 date := sysdate;
+begin
+ select count(*) into c from t1, t2, t3
+ where x1 = v1 or x2 = v2 or x3 = v3;
+end;
+/
+show errors
+--
+exec pl;
+--
+exec pb;
+--
+exit
